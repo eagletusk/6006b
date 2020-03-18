8d2ff5ee4002c3209c563b9ad153d17dfa39bd20
Paul Sobczak <sobczak.paul@gmail.com>
2020-03-18 05:01:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+   //console.log(nums2);
+    for (var i = 0; i<nums2.length-1; i++){
+       console.log(nums1);
+      for(var j= 0; j<nums1.length; j++){
+        var abc = nums1.splice(1,j,8);
+        var bbc = nums1.splice(j+1,1,abc);
+        console.log(nums1);
+         console.log('a');
+      }
+    }
+};
+
+
+
+var nums1 = [1,2,3,0,0,0];
+var m = 3;
+var nums2 = [2,5,6];
+var n = 3;
+merge(nums1,m,nums2,n);
+
+const merge = (nums1, m, nums2, n) => {
+  let count = 0;   
+    for (let i = 0; i < nums1.length; i++) {
+      if (nums1[i] === 0) {
+        count++;
+        if (count == n) {
+          nums1.splice(nums1.length - n);
+        }
+      }
+    }
+    nums1.push(...nums2);
+    nums1.sort((a, b) => a -b);
+};
